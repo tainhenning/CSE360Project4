b1e11b0947843938d452fdafc3489976b9261b46
mpardi <mpardi@asu.edu>
2017-11-09 15:58:08 -0700

    Updates to ControlCenter

--- src/ControlCenter.java
+++ src/ControlCenter.java
@@ -15,29 +15,47 @@ public class ControlCenter {
 		return _instance;
 	}
 	
-	private int[] correctCount = new int[7];
-	private int[] incorrectCount = new int[7];
-	private float[] timeSpent;
+	private int[] correctCount = new int[8];
+	private int[] incorrectCount = new int[8];
+	private float[] timeSpentPerQuestion;
+	private float[][] timeSpentPerLessons;
 	
 	private int currentLesson;
 	
+	private int currentQuestion;
+	
 	public void setCurrentLesson(int currentLesson) {
 		this.currentLesson = currentLesson;
+		currentQuestion = 0;
 	}
 	
 	public int getCurrentLesson() {
 		return currentLesson;
 	}
 	
+	public void setCurrentQuestion(int currentQuestion) {
+		this.currentQuestion = currentQuestion;
+	}
+	
+	public int getCurrentQuestion() {
+		return currentQuestion;
+	}
+	
 	public void answerCorrect() {
 		correctCount[currentLesson]++;
+		for (int i = 0; i < correctCount.length; i++) {
+			System.out.print(correctCount[i]);
+		}
 	}
 	
 	public void answerIncorrect() {
 		incorrectCount[currentLesson]++;
+		for (int i = 0; i < correctCount.length; i++) {
+			System.out.print(incorrectCount[i]);
+		}
 	}
 	
 	public void timeSet(float time) {
-		timeSpent[currentLesson] = time;
+		
 	}
 }
